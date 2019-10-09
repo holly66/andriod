28c066319857af358190002dc4e934f5831d9ef8
holly66 <854633543@qq.com>
2019-10-09 14:01:43 +0800

    布局

---
+++ constraintLayout/app/src/main/java/com/example/administrator/constraintlayout/constraintlayout.java
@@ -0,0 +1,13 @@
+package com.example.administrator.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class constraintlayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraintlayout);
+    }
+}
